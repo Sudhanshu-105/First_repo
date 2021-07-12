19dc0d2af1f65b67dd31a1f58e6d2c02c27be61d
Sudhanshu <imsudhanshu3@gmail.com>
2021-07-12 16:10:09 +0530

    Sum and multiply

---
+++ multiply.cpp
@@ -0,0 +1,19 @@
+#include <iostream>  
+
+using namespace std;
+ 
+
+int main() 
+{
+   
+int a = 12;
+   
+int b = 7;
+ 
+   
+int product = a * b;
+ 
+   
+cout << product << endl;
+
+}
---
+++ sum.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main()
+
+{
+	
+int a,b,c;
+
+	cout<< "\nEnter first number : ";
+	
+cin>>a;
+	cout<<"\nEnter second number : ";
+	
+cin>>b;
+	c=a+b;
+	cout<<"\nThe Sum is : "<<c;
+	
+	
+	
+return 0;
+
+}
